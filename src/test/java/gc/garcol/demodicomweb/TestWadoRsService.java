ffe1297549668b3995754554335cfc6cba63845c
Thai Van <giacatluong20121996@gmail.com>
2021-12-21 10:55:13 +0700

    [TEST] WadoRSService - handle callback

--- src/test/java/gc/garcol/demodicomweb/TestWadoRsService.java
+++ src/test/java/gc/garcol/demodicomweb/TestWadoRsService.java
@@ -1,7 +1,9 @@
 package gc.garcol.demodicomweb;
 
+import gc.garcol.demodicomweb.service.StowRsService;
 import gc.garcol.demodicomweb.service.WadoRsService;
 import gc.garcol.demodicomweb.service.model.WadoRsQry;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +13,7 @@ import java.util.function.Consumer;
 /**
  * @author garcol
  */
+@Slf4j
 public class TestWadoRsService {
 
     @Test
@@ -31,6 +34,29 @@ public class TestWadoRsService {
         };
         wadoRsService.retrieveStudy(url, wadoRsQry, callback);
 
+    }
+
+    @Test
+    public void testRetrieveStudiesV2() {
+        RestTemplate restTemplate = new RestTemplate();
+        WadoRsService wadoRsService = new WadoRsService(restTemplate);
+
+        String url = "http://127.0.0.1:8080/dcm4chee-arc";
+        String studyInstanceUID = "1.3.12.2.1107.5.4.3.4975316777216.19951114.94101.16";
+        WadoRsQry wadoRsQry = new WadoRsQry()
+                .setAet("DCM4CHEE")
+                .setStudyInstanceUID(studyInstanceUID)
+                ;
+
+        Consumer<InputStream> callback = (inputStream) -> {
+            try {
+                System.out.println("=------------- on callback");
+                StowRsService.newInstance().uploadDicom(inputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        wadoRsService.retrieveStudy(url, wadoRsQry, callback);
     }
 
 }
